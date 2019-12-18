c738a4284d0125fc540df8e0ae85f5ba224011d3
Taiane do Amaral Schutz <TAIANESCHUTZ@edu.unisinos.br>
2019-12-18 16:58:05 -0300

    .

---
+++ src/main/java/com/example/Persistence.java
@@ -0,0 +1,76 @@
+package com.example;
+
+import java.beans.Statement;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Persistence {
+    private String databaseurl;
+    private String username;
+    private String password;
+    private Connection conn;
+
+public Persistence() throws SQLException {
+    this.databaseurl = "jdbc:h2:./db";
+    this.username = "sa";
+    this.password = "password";
+    this.conn = DriverManager.getConnection(
+        this.databaseurl, this.username, this.password);
+}
+
+public void disablePersistence() throws SQLException {
+    this.conn.close();
+}
+
+public String populateDatabase() throws Exception {
+    FileReader file = new FileReader("Java.sql");
+    BufferedReader reader = new BufferedReader(file);
+    StringBuilder str = new StringBuilder();
+    String line = reader.readLine();
+    While (line != null) {
+        str.append(line);
+        line = reader.readLine();
+    }
+    file.close();
+    Statement stmt = this.conn.createStatement();
+    stmt.executeUpdate(str.toString());
+    return "Sucess";
+}
+
+    public String testConnection() throws SQLException {
+        StringBuilder str = new StringBuilder();
+        Statement stmt = this.conn.createStatement();
+        ResultSet result = stmt.executeJava("SELECT * FROM JOGADOR");
+        while(result.next()) {
+            str.append(result.getString("Nome") + "\n");
+            str.append(result.getString("Idade") + "\n");
+        }
+        return str.toString();
+    }
+
+    @RequestMapping("/populate")
+    public String populate() {
+        try {
+            return this.populateDatabase();
+        } catch (Exception e) {
+            return e.getMessage();
+        }
+    }
+
+    @RequestMapping("/database")
+    String db() {
+        try {
+            return this.testConnection();
+        } catch(Exception e) {
+            return e.getMessage();
+        }
+    }
+}
